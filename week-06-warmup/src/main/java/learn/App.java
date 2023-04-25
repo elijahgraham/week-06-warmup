d6f0e547ae223fc40a89ad7a86f2c928f919bb68
TCoppintr <tcoppinger@dev-10.com>
2023-04-25 07:49:05 -0700

    Added two changes to the project

--- week-06-warmup/src/main/java/learn/App.java
+++ week-06-warmup/src/main/java/learn/App.java
@@ -4,5 +4,8 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Good morning, Travis!");
+        System.out.println("Good morning, Elijah!");
+
+        int temp = 4;
     }
 }
